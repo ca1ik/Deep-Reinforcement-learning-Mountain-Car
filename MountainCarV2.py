f3cb12c6aac0c3d170ca1e8106f28f5e3073dbdd
Halil Calik <halicalix@gmail.com>
2024-11-08 01:31:34 +0300

    Update MountainCarV2.py

--- MountainCarV2.py
+++ MountainCarV2.py
@@ -1,187 +1,130 @@
-import gym
-from keras import models
-from keras import layers
-from keras.optimizers import Adam
-from collections import deque
-import random
-import numpy as np
+    import gym
+    import tensorflow as tf
+    import keras
+    from tensorflow.keras import models
+    from tensorflow.keras import layers
+    from tensorflow.keras.optimizers import Adam
+    from collections import deque
+    import random
+    import numpy as np
+
+    gpus = tf.config.list_physical_devices('GPU')
+    if gpus:
+        try:
+            # GPU configuration
+            tf.config.set_virtual_device_configuration(
+                gpus[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=4096)])  # 4GB memory limit
+            print("GPU is being used.")
+        except RuntimeError as e:
+            print(e)
+    else:
+        print("GPU not found. Using CPU instead.")
 
 
-#############################
-#if you want to use GPU to boost, use these code.  
+    class MountainCarTrain:
+        def __init__(self, env):
+            self.env = env
+            self.gamma = 0.99
+            self.epsilon = 1
+            self.epsilon_decay = 0.05
+            self.epsilon_min = 0.01
+            self.learningRate = 0.001
+            self.replayBuffer = deque(maxlen=20000)
+            self.trainNetwork = self.createNetwork()
+            self.episodeNum = 400
+            self.iterationNum = 201  # max is 200
+            self.numPickFromBuffer = 32
+            self.targetNetwork = self.createNetwork()
+            self.targetNetwork.set_weights(self.trainNetwork.get_weights())
 
-# import tensorflow as tf
-# import keras
-# config = tf.ConfigProto( device_count = {'GPU': 2 , 'CPU': 1} ) 
-# sess = tf.Session(config=config) 
-# keras.backend.set_session(sess)
+        def createNetwork(self):
+            model = models.Sequential()
+            state_shape = self.env.observation_space.shape
+            model.add(layers.Dense(24, activation='relu', input_shape=state_shape))
+            model.add(layers.Dense(48, activation='relu'))
+            model.add(layers.Dense(self.env.action_space.n, activation='linear'))
+            model.compile(loss='mse', optimizer=Adam(learning_rate=self.learningRate))
+            return model
 
-#############################
-
-
-class MountainCarTrain:
-    def __init__(self,env):
-        self.env=env
-        self.gamma=0.99
-
-        self.epsilon = 1
-        self.epsilon_decay = 0.05
-
-        self.epsilon_min=0.01
-
-
-        self.learingRate=0.001
-
-        self.replayBuffer=deque(maxlen=20000)
-        self.trainNetwork=self.createNetwork()
-
-        self.episodeNum=400
-
-        self.iterationNum=201 #max is 200
-
-        self.numPickFromBuffer=32
-
-        self.targetNetwork=self.createNetwork()
-
-        self.targetNetwork.set_weights(self.trainNetwork.get_weights())
-
-    def createNetwork(self):
-        model = models.Sequential()
-        state_shape = self.env.observation_space.shape
-
-        model.add(layers.Dense(24, activation='relu', input_shape=state_shape))
-        model.add(layers.Dense(48, activation='relu'))
-        model.add(layers.Dense(self.env.action_space.n,activation='linear'))
-        # model.compile(optimizer=optimizers.RMSprop(lr=self.learingRate), loss=losses.mean_squared_error)
-        model.compile(loss='mse', optimizer=Adam(lr=self.learingRate))
-        return model
-
-    def getBestAction(self,state):
-
-        self.epsilon = max(self.epsilon_min, self.epsilon)
-
-        if np.random.rand(1) < self.epsilon:
-            action = np.random.randint(0, 3)
-        else:
-            action=np.argmax(self.trainNetwork.predict(state)[0])
-
-        return action
-
-    
-
-    def trainFromBuffer_Boost(self):
-        if len(self.replayBuffer) < self.numPickFromBuffer:
-            return
-        samples = random.sample(self.replayBuffer,self.numPickFromBuffer)
-        npsamples = np.array(samples)
-        states_temp, actions_temp, rewards_temp, newstates_temp, dones_temp = np.hsplit(npsamples, 5)
-        states = np.concatenate((np.squeeze(states_temp[:])), axis = 0)
-        rewards = rewards_temp.reshape(self.numPickFromBuffer,).astype(float)
-        targets = self.trainNetwork.predict(states)
-        newstates = np.concatenate(np.concatenate(newstates_temp))
-        dones = np.concatenate(dones_temp).astype(bool)
-        notdones = ~dones
-        notdones = notdones.astype(float)
-        dones = dones.astype(float)
-        Q_futures = self.targetNetwork.predict(newstates).max(axis = 1)
-        targets[(np.arange(self.numPickFromBuffer), actions_temp.reshape(self.numPickFromBuffer,).astype(int))] = rewards * dones + (rewards + Q_futures * self.gamma)*notdones
-        self.trainNetwork.fit(states, targets, epochs=1, verbose=0)
-
-
-
-    def trainFromBuffer(self):
-        if len(self.replayBuffer) < self.numPickFromBuffer:
-            return
-
-        samples = random.sample(self.replayBuffer,self.numPickFromBuffer)
-
-        states = []
-        newStates=[]
-        for sample in samples:
-            state, action, reward, new_state, done = sample
-            states.append(state)
-            newStates.append(new_state)
-
-        newArray = np.array(states)
-        states = newArray.reshape(self.numPickFromBuffer, 2)
-
-        newArray2 = np.array(newStates)
-        newStates = newArray2.reshape(self.numPickFromBuffer, 2)
-
-        targets = self.trainNetwork.predict(states)
-        new_state_targets=self.targetNetwork.predict(newStates)
-
-        i=0
-        for sample in samples:
-            state, action, reward, new_state, done = sample
-            target = targets[i]
-            if done:
-                target[action] = reward
+        def getBestAction(self, state):
+            self.epsilon = max(self.epsilon_min, self.epsilon)
+            if np.random.rand(1) < self.epsilon:
+                action = np.random.randint(0, 3)
             else:
-                Q_future = max(new_state_targets[i])
-                target[action] = reward + Q_future * self.gamma
-            i+=1
+                action = np.argmax(self.trainNetwork.predict(state)[0])
+            return action
 
-        self.trainNetwork.fit(states, targets, epochs=1, verbose=0)
+        def trainFromBuffer(self):
+            if len(self.replayBuffer) < self.numPickFromBuffer:
+                return
+            samples = random.sample(self.replayBuffer, self.numPickFromBuffer)
+            states, actions, rewards, newstates, dones = zip(*samples)
+            states = np.array(states).reshape(self.numPickFromBuffer, 2)
+            newstates = np.array(newstates).reshape(self.numPickFromBuffer, 2)
+
+            targets = self.trainNetwork.predict(states)
+            new_state_targets = self.targetNetwork.predict(newstates)
+
+            for i in range(self.numPickFromBuffer):
+                target = targets[i]
+                if dones[i]:
+                    target[actions[i]] = rewards[i]
+                else:
+                    Q_future = max(new_state_targets[i])
+                    target[actions[i]] = rewards[i] + Q_future * self.gamma
+
+            self.trainNetwork.fit(states, targets, epochs=1, verbose=0)
+
+        def orginalTry(self, currentState, eps):
+            rewardSum = 0
+            max_position = -99
+
+            for i in range(self.iterationNum):
+                bestAction = self.getBestAction(currentState)
+
+                # Show the animation every 50 episodes
+                if eps % 50 == 0:
+                    self.env.render()
+
+                # Adjust the unpacking to handle the new 'info' returned by env.step()
+                new_state, reward, done, truncated, info = self.env.step(bestAction)  # Updated line
+                new_state = new_state.reshape(1, 2)
+
+                # Keep track of max position
+                if new_state[0][0] > max_position:
+                    max_position = new_state[0][0]
+
+                # Adjust reward for task completion
+                if new_state[0][0] >= 0.5:
+                    reward += 10
+
+                self.replayBuffer.append([currentState, bestAction, reward, new_state, done])
+
+                # Train from buffer
+                self.trainFromBuffer()
+
+                rewardSum += reward
+                currentState = new_state
+
+                if done:
+                    break
+
+                print(f"Episode {eps} finished with reward: {rewardSum}, max position: {max_position}")
+                print(f"Total reward: {rewardSum}, max position achieved: {max_position}")
+
+                # Sync the target network
+                self.targetNetwork.set_weights(self.trainNetwork.get_weights())
+
+                print(f"Now epsilon is {max(self.epsilon_min, self.epsilon)}, the reward is {rewardSum}, maxPosition is {max_position}")
+                self.epsilon -= self.epsilon_decay
+
+        def start(self):
+            for eps in range(self.episodeNum):
+                # Reset environment and get the initial state
+                currentState = self.env.reset()[0].reshape(1, 2)  # observation'ı doğru şekilde almak için [0] eklenmeli
+                self.orginalTry(currentState, eps)
 
 
-    def orginalTry(self,currentState,eps):
-        rewardSum = 0
-        max_position=-99
-
-        for i in range(self.iterationNum):
-            bestAction = self.getBestAction(currentState)
-
-            #show the animation every 50 eps
-            if eps%50==0:
-                env.render()
-
-            new_state, reward, done, _ = env.step(bestAction)
-
-            new_state = new_state.reshape(1, 2)
-
-            # # Keep track of max position
-            if new_state[0][0] > max_position:
-                max_position = new_state[0][0]
-
-
-            # # Adjust reward for task completion
-            if new_state[0][0] >= 0.5:
-                reward += 10
-
-            self.replayBuffer.append([currentState, bestAction, reward, new_state, done])
-
-            #Or you can use self.trainFromBuffer_Boost(), it is a matrix wise version for boosting 
-            self.trainFromBuffer()
-
-            rewardSum += reward
-
-            currentState = new_state
-
-            if done:
-                break
-
-        if i >= 199:
-            print("Failed to finish task in epsoide {}".format(eps))
-        else:
-            print("Success in epsoide {}, used {} iterations!".format(eps, i))
-            self.trainNetwork.save('./trainNetworkInEPS{}.h5'.format(eps))
-
-        #Sync
-        self.targetNetwork.set_weights(self.trainNetwork.get_weights())
-
-        print("now epsilon is {}, the reward is {} maxPosition is {}".format(max(self.epsilon_min, self.epsilon), rewardSum,max_position))
-        self.epsilon -= self.epsilon_decay
-
-    def start(self):
-        for eps in range(self.episodeNum):
-            currentState=env.reset().reshape(1,2)
-            self.orginalTry(currentState, eps)
-
-
-env = gym.make('MountainCar-v0')
-dqn=MountainCarTrain(env=env)
-dqn.start()
-
-
-
+    env = gym.make('MountainCar-v0')
+    dqn = MountainCarTrain(env=env)
+    dqn.start()
